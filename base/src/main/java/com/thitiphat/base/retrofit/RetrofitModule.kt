859e0ba6247a63336273137c10f04a1d2cbc0cf6
Pattiiz <phat.my.name@gmail.com>
2022-11-30 21:49:43 +0700

    [Network] create RetrofitModule

---
+++ base/src/main/java/com/thitiphat/base/retrofit/RetrofitModule.kt
@@ -0,0 +1,18 @@
+package com.thitiphat.base.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitModule {
+
+    private val BASE_URL = "https://api.openweathermap.org/"
+
+    fun init(): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}

ff9d88143d2d12f887ad96fb8ecacd3d055443eb
Pattiiz <phat.my.name@gmail.com>
2022-11-30 21:54:11 +0700

    [MainApp] replace findViewById with viewBinding

--- app/src/main/java/com/thitiphat/krungsritest/MainActivity.kt
+++ app/src/main/java/com/thitiphat/krungsritest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.thitiphat.krungsritest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thitiphat.krungsritest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
 }

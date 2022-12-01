393c4b3e0fbdee4c73e98602d306967fb1036de8
Pattiiz <phat.my.name@gmail.com>
2022-12-01 12:25:07 +0700

    [Setup] add ForecastKoinModule to MainApplication

--- app/src/main/java/com/thitiphat/krungsritest/KrungsriTestApplication.kt
+++ app/src/main/java/com/thitiphat/krungsritest/KrungsriTestApplication.kt
@@ -1,15 +1,23 @@
 package com.thitiphat.krungsritest
 
 import android.app.Application
-import com.thitiphat.base.di.KoinModule
+import com.thitiphat.base.di.BaseKoinModule
+import com.thitiphat.forecast.di.ForecastKoinModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class KrungsriTestApplication: Application() {
+class KrungsriTestApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            modules(KoinModule().baseModule)
+            androidContext(this@KrungsriTestApplication)
+            modules(
+                listOf(
+                    BaseKoinModule().koinModules,
+                    ForecastKoinModule().koinModules
+                )
+            )
         }
     }
 

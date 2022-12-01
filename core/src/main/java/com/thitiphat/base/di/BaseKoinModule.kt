70c20de0c10f62932a433add0785e5fef39269ad
Pattiiz <phat.my.name@gmail.com>
2022-12-01 12:18:53 +0700

    [Core] rename KoinModule to BaseKoinModule

--- core/src/main/java/com/thitiphat/base/di/BaseKoinModule.kt
+++ core/src/main/java/com/thitiphat/base/di/BaseKoinModule.kt
@@ -3,9 +3,12 @@ package com.thitiphat.base.di
 import com.thitiphat.base.retrofit.RetrofitModule
 import org.koin.dsl.module
 
-class KoinModule {
+class BaseKoinModule {
 
-    val baseModule = module {
+    private val networkModule = module {
         single { RetrofitModule.init() }
     }
+
+    val koinModules = module { includes(networkModule) }
+
 }

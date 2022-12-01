549a6e145db98018a4ee6666db940fd8e104fa04
Pattiiz <phat.my.name@gmail.com>
2022-12-01 12:15:55 +0700

    [Forecast] AllDayForecastFragment - rename package

--- feature/forecast/src/main/java/com/thitiphat/forecast/presentation/AllDayForecastFragment.kt
+++ feature/forecast/src/main/java/com/thitiphat/forecast/presentation/AllDayForecastFragment.kt
@@ -1,11 +1,11 @@
-package com.thitiphat.feature_alldayforecast.presentation
+package com.thitiphat.forecast.presentation
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.thitiphat.feature_alldayforecast.databinding.FragmentAllDayForecastBinding
+import com.thitiphat.forecast.databinding.FragmentAllDayForecastBinding
 
 class AllDayForecastFragment : Fragment() {
 

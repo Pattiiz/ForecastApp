fed1bcf2200b5cd9c9290b70b739ce7b75431210
Pattiiz <phat.my.name@gmail.com>
2022-12-02 13:26:48 +0700

    [Forecast] add usecase and viewmodel to ForecastKoinModule

--- feature/forecast/src/main/java/com/thitiphat/forecast/di/ForecastKoinModule.kt
+++ feature/forecast/src/main/java/com/thitiphat/forecast/di/ForecastKoinModule.kt
@@ -2,6 +2,10 @@ package com.thitiphat.forecast.di
 
 import com.thitiphat.forecast.data.repository.Repository
 import com.thitiphat.forecast.data.repository.RepositoryImp
+import com.thitiphat.forecast.domain.GetCurrentWeatherUseCase
+import com.thitiphat.forecast.domain.GetForecastUseCase
+import com.thitiphat.forecast.presentation.WeatherViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 class ForecastKoinModule {
@@ -14,5 +18,17 @@ class ForecastKoinModule {
         factory<Repository> { RepositoryImp(get()) }
     }
 
-    val koinModules = module { includes(apiModule, repositoryModule)}
+    private val viewModelModule = module {
+        viewModel { WeatherViewModel(get(), get()) }
+    }
+
+    private val useCaseModule = module {
+        factory { GetCurrentWeatherUseCase(get()) }
+        factory { GetForecastUseCase(get()) }
+    }
+
+    val koinModules =
+        module {
+            includes(apiModule, repositoryModule, viewModelModule, useCaseModule)
+        }
 }

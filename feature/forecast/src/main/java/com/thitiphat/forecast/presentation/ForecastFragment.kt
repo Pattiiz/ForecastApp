d6ff92fc05f8c41cc7cc076d3fd11bc04e0d89c1
Pattiiz <phat.my.name@gmail.com>
2022-12-04 12:54:42 +0700

    [Forecast] create ForecastFragment

---
+++ feature/forecast/src/main/java/com/thitiphat/forecast/presentation/ForecastFragment.kt
@@ -0,0 +1,142 @@
+package com.thitiphat.forecast.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.fragment.app.Fragment
+import com.thitiphat.forecast.R
+import androidx.navigation.fragment.navArgs
+import com.thitiphat.forecast.databinding.FragmentForecastBinding
+import com.thitiphat.core.util.TemperatureConversionUtil
+import com.thitiphat.core.util.TimeConversionUtil
+import com.thitiphat.data.forecast.constant.Constant
+import com.thitiphat.data.forecast.model.ForecastModel
+import com.thitiphat.data.forecast.model.ForecastResponseModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+class ForecastFragment : Fragment() {
+
+    private val binding: FragmentForecastBinding by lazy {
+        FragmentForecastBinding.inflate(
+            layoutInflater
+        )
+    }
+
+    private val args: ForecastFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding.composeView.apply {
+            setContent {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+                AllDayForecastScreen(args.forecastData, args.cityName)
+
+            }
+        }
+        return binding.root
+    }
+
+    @Composable
+    fun AllDayForecastScreen(data: ForecastResponseModel, cityName: String) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = cityName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 16.dp),
+                fontSize = 42.sp
+            )
+            Text(
+                text = SimpleDateFormat(stringResource(id = R.string.dd_MM_yyy_HH_mm)).format(
+                    Calendar.getInstance().time
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, top = 8.dp),
+                fontSize = 26.sp
+            )
+            AllDayForecast(data = data.list)
+        }
+    }
+
+    @Composable
+    fun AllDayForecast(data: List<ForecastModel>) {
+        val isCelsius = remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            ) {
+                items(data) { eachItem ->
+                    EachHours(eachItem, isCelsius.value)
+                }
+            }
+            OutlinedButton(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+                    .padding(horizontal = 32.dp),
+                onClick = { isCelsius.value = !isCelsius.value }
+            ) {
+                Text(
+                    text = if (isCelsius.value) stringResource(id = R.string.forecast_change_to_fahrenheit)
+                    else stringResource(id = R.string.forecast_change_to_celsius),
+                )
+            }
+        }
+
+    }
+
+    @Composable
+    fun EachHours(data: ForecastModel, isCelsius: Boolean) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .background(color = Color(236, 239, 241), RoundedCornerShape(4.dp))
+                .padding(16.dp)
+        ) {
+            data.dt?.let {
+                Text(
+                    text = TimeConversionUtil.unixTimeToGmt(it)
+                )
+            }
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = Constant.STRING_EMPTY
+            )
+            Text(text = if (isCelsius) TemperatureConversionUtil.fahrenheitToCelsius(data.main?.temp.toString()) else data.main?.temp.toString())
+        }
+    }
+}
